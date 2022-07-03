bd887b37745d9654ef0072d49735249b72c42855
kodyberry23 <kodyberry23@gmail.com>
2022-07-03 12:39:03 -0600

    adding controller and router for products

--- lib/poeticoins.ex
+++ lib/poeticoins.ex
@@ -1,4 +1,5 @@
 defmodule Poeticoins do
+  defdelegate available_products(), to: Poeticoins.Exchanges
   defdelegate subscribe_to_trades(product), to: Poeticoins.Exchanges, as: :subscribe
   defdelegate unsubscribe_from_product(product), to: Poeticoins.Exchanges, as: :unsubscribe
   defdelegate get_last_trade(product), to: Poeticoins.Historical
